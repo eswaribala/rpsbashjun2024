eaaff42d0316e87c809167c87851c2d33fc6c8f4
eswaribala <Parameswaribala@gmail.com>
2024-06-11 15:46:11 +0530

    updated

---
+++ customerarray.sh
@@ -0,0 +1,11 @@
+#create array of names
+names=("Param" "Bala" "Vignesh" "Shubha")
+#show length of the array
+echo  ${#names[@]}
+#show all the elements of the array
+echo ${names[@]}
+#alternative way to show all the elements of the array
+echo ${names[*]}
+
+
+
